da2d40d4ab3268037c212f74f52246cf1f1a1186
Benson <bensonmwanake79@gmail.com>
2025-07-23 09:28:45 +0300

    update the app file

--- Group-6-project/src/App.jsx
+++ Group-6-project/src/App.jsx
@@ -1,11 +1,19 @@
-
+import { Routes, Route } from "react-router-dom"
+import Navbar from "./Components/Navbar"
+import Home from "./Pages/Home"
+import AddItem from "./Pages/AddItem"
+import ItemDetails from "./Pages/ItemDetails"
 
 function App() {
-
   return (
-    <>
-      
-    </>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<AddItem />} />
+        <Route path="/items/:id" element={<ItemDetails />} />
+      </Routes>
+    </div>
   )
 }
 
