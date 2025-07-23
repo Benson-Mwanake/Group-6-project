17edb437035815abe88d111b178c6d2443b75a8f
+ <+githinjiadreen27@gmail.com>
2025-07-23 09:26:38 +0300

    FilterBar Component

--- Group-6-project/src/Components/FilterBar.jsx
+++ Group-6-project/src/Components/FilterBar.jsx
@@ -5,6 +5,7 @@ function FilterBar({filter, setFilter}) {
     <div className='filter-bar'>
         <button onClick={() => setFilter('All')} className={filter === 'All' ? 'active' : ''}>All</button>
         <button onClick={() => setFilter('Lost')} className={filter === 'Lost' ? 'active' : ''}>Lost</button>
+        <button onClick={() => setFilter('Found')} className={filter === 'Found' ? 'active' : ''}>Found</button>
     </div>
   )
 }
