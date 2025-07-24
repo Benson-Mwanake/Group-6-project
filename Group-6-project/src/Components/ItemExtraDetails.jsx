01832dea35b3532041b7801a124b269bd45ec4ec
Benson <bensonmwanake79@gmail.com>
2025-07-24 10:32:42 +0300

    fixed some style errors

--- Group-6-project/src/Components/ItemExtraDetails.jsx
+++ Group-6-project/src/Components/ItemExtraDetails.jsx
@@ -3,10 +3,10 @@ import '../App.css'
 function ItemExtraDetails({ item }) {
   return (
      <div className="item-card "id='item-card3'>
+      <img src={item.image} alt={item.title} />
       <h3>{item.title}</h3>
       <p><strong>Type:</strong> {item.type}</p>
       <p><strong>Location:</strong> {item.location}</p>
-      <img src={item.image} alt={item.title} />
       <p>{item.description}</p>
       <p><strong>Contact:</strong> {item.contactInfo}</p>
       <p><strong>Day:</strong> {item.DateLost} <strong>Time:</strong> {item.TimeLost}</p>
